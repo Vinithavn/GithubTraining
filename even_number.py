3ebeb6a2d986e0a2a7103ab28495fee27d54274f
Vinithavn <vinithavn01@gmail.com>
2025-12-16 16:23:05 +0530

    Updated the sample list

--- even_number.py
+++ even_number.py
@@ -2,5 +2,5 @@ def return_even_numbers(lst):
   return [num for num in lst if num % 2 == 0]
 
 if __name__ == "__main__":
-  sample_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 110]
-  print("Even numbers in the list:", find_even_numbers(sample_list))
+  sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  print("Even numbers in the list:", return_even_numbers(sample_list))
