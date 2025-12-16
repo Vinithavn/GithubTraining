e47d75b33b3b5470b3f75eb85b60fbea0a794707
Vinitha V N <57786021+Vinithavn@users.noreply.github.com>
2025-12-16 16:21:59 +0530

    Rename function and update sample list

--- even_number.py
+++ even_number.py
@@ -1,6 +1,6 @@
-def return_even_numbers(lst):
-  return [num for num in lst if num % 2 == 0]
+def even_numbers_after_5(lst):
+  return [num for num in lst if num % 2 == 0 and num>5]
 
 if __name__ == "__main__":
-  sample_list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 110]
+  sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   print("Even numbers in the list:", find_even_numbers(sample_list))
