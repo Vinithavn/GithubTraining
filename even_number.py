9ea38b98893fb711b55bff9079be2d29131f2f02
Vinithavn <vinithavn01@gmail.com>
2025-12-23 14:59:26 +0530

    Added return statement

--- even_number.py
+++ even_number.py
@@ -3,6 +3,7 @@ def find_even_numbers(lst):
   for i in lst:
     if i % 2 == 0:
       new_list.append(i)
+  return new_list
 
 if __name__ == "__main__":
   sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
