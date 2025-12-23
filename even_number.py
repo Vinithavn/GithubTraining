f18ef05c43cbb8e45cf48e268a7287f78411e655
Vinithavn <vinithavn01@gmail.com>
2025-12-23 15:44:35 +0530

    commit 1

--- even_number.py
+++ even_number.py
@@ -4,7 +4,7 @@ def find_even_numbers(lst):
     if i % 2 == 0:
       new_list.append(i)
   print("Removing a messy commit 2")
-  a=20
+  a="commit 1"
   return new_list
 
 if __name__ == "__main__":
